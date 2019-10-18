f458f150a44d7adfeb490949e6ec6d122c2a84dd
zachary-peasley <zpman95@hotmail.com>
2019-10-18 10:13:40 -0500

    require deps

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs')
+const Users = require('../users/users-model');
+const jwt = require('jsonwebtoken');
 
 router.post('/register', (req, res) => {
   // implement registration
