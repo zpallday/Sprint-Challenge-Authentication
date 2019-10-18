2f38a154cba5292458a00a808f6f284e58152df8
zachary-peasley <zpman95@hotmail.com>
2019-10-18 11:54:04 -0500

    fixed errors

--- auth/auth-router.test.js
+++ auth/auth-router.test.js
@@ -1,11 +1,14 @@
-const request = require('supertest')
-const db = require('../database/dbConfig')
-const server = require('../api/server.js')
+  
+const request = require('supertest');
+
+const db = require('../database/dbConfig.js');
+const server = require('../api/server.js');
 
 describe('auth router', () => {
     beforeEach(async () => {
-    await db('users').truncate();
+        await db('users').truncate();
     });
+
     describe('POST /api/auth/register', () => {
         it('returns status 201', async () => {
             return request(server)
@@ -22,7 +25,20 @@ describe('auth router', () => {
                 .first();
             expect(newUser.username).toBe('John')
         });
-  
+    });
+
+    describe('POST api/auth/login', () => {
+        it('returns status 200', async () => {
+            await request(server)
+                .post('/api/auth/register')
+                .send({username: 'John', password: 'banana'})
+            return request(server)
+                .post('/api/auth/login')
+                .send({username: 'John', password: 'banana'})
+            .then(res => {
+                expect(res.status).toBe(200)
+            });
+        });
         it('returns a token on successful login', async () => {
             await request(server)
                 .post('/api/auth/register')
