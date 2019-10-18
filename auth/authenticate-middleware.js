8d96626a183a521800be6d9bc1824fcae44f40ad
zachary-peasley <zpman95@hotmail.com>
2019-10-18 10:20:22 -0500

    middleware updated

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,18 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require('jsonwebtoken')
+const secrets = require('../config/secrets')
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, secrets.jwtSecret, (error, decodedToken) => {
+    if (error) {
+      res.status(401).json({ you: 'shall not pass!' });
+    } else {
+      req.user = {username: decodedToken.username};
+      next()
+    }
+    })
+  } else {
+    res.status(400).json({message: "please provide credentials"})
+  }
 };
