2e906a5f30f097a65f457c21d668600c5dbe36e8
zachary-peasley <zpman95@hotmail.com>
2019-10-18 11:13:23 -0500

    Testing on jokes works

--- jokes/jokes-router.test.js
+++ jokes/jokes-router.test.js
@@ -7,6 +7,38 @@ describe('jokes router', () => {
     await db('users').truncate();
     });
 
-  
+  describe('GET /api/jokes without auth', () => {
+      it('returns status 400', () => {
+          return request(server).get('/api/jokes')
+          .then(res => {
+              expect(res,status).toBe(400)
+          })
+      })
+  });
+
+  describe('GET /api/jokes with authorization', () => {
+      it('returns status 200', async () => {
+        await request(server)
+          .post('api/auth/register')
+          .send({ username: 'Jim', password: "peanut"});
+        await request(server)
+        .post('api/auth/jokes')
+        .send({ username: 'Jim', password: "peanut"})
+        .then(res => {
+        const {token} = JSON.parse(res.text)
+        console.log(token)
+        return token
+        })
+        .then (async token => {
+            const response = await request(server)
+            .get('api/jokes')
+            .set('authorization', token)
+            return response
+        })
+        .then(res => {
+            expect(res.status).toBe(200)
+        })
+      })
+  })
 
 })
