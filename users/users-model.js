c3e449e9b142e00af48b18762560714a7498cd67
zachary-peasley <zpman95@hotmail.com>
2019-10-18 10:10:18 -0500

    created model

---
+++ users/users-model.js
@@ -0,0 +1,25 @@
+const db = require('../database/dbConfig');
+
+module.exports = {
+    add,
+    findBy,
+    findById
+}
+
+function findBy(filter) {
+    return db('users')
+    .where(filter)
+};
+
+async function add(user) {
+    const [id] = await db('users')
+    .insert(user);
+
+    return findById(id)
+}
+
+function findById(id) {
+    return db('users')
+    .where({ id })
+    .first()
+}
